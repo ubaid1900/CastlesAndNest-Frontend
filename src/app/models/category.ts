b220bb9be5436986b920c0fca9d39517a5fc52c8
Ubaid Mohammed <ubaid1900@gmail.com>
2021-10-15 23:59:00 +0530

    cat subcat image

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,10 +1,12 @@
 export interface Category {
     id: number;
     name: string;
+    imageUrl: string;
 }
 export interface SubCategory {
     id: number;
     categoryId: number;
     category: Category;
     name: string;
+    imageUrl: string;
 }
