bde9827b0ab013ffc714e5f6e814b9f969431e31
Ubaid Mohammed <ubaid1900@gmail.com>
2021-11-13 19:44:53 +0530

    center align p

--- src/app/about-us/about-us/about-us.component.ts
+++ src/app/about-us/about-us/about-us.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MiscService } from 'src/app/services/misc.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutUsComponent implements OnInit {
   aboutUsTexts: string[] = [];
