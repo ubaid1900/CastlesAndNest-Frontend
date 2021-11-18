42fe5feee7f40c497e39cf1a1c951a8e914b2466
Ubaid Mohammed <ubaid1900@gmail.com>
2021-11-18 15:53:56 +0530

    documentation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://castlesandnestapi.elhamd.in/api/',
+  apiUrl: 'https://castlesandnestapi.elhamd.in/api/', /* this should end with slash */
   carouselInterval: 3000,
   googleAuthSettings: {
     "clientId": "71954085878-1sgkrm6ce4bhghfi6tsrd9qevcmkij7n.apps.googleusercontent.com"
