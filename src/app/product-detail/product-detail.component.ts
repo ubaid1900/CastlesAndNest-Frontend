01d4a70f7a37be1434bcd6a89960497a3b5a912a
Ubaid Mohammed <ubaid1900@gmail.com>
2021-11-26 20:27:34 +0530

    whatsapp app launch fix

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { Product } from '../models/Product';
@@ -12,17 +13,19 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class ProductDetailComponent implements OnInit {
   product!: Product;
+  safeUrl!: SafeUrl
   public carouselInterval = 0;
   @ViewChild('carousel', { static: false }) carousel!: NgbCarousel;
 
   constructor(private route: ActivatedRoute, public router: Router
-    , public authenticationService: AuthenticationService, public location: Location) { }
+    , public authenticationService: AuthenticationService, public location: Location,
+    private dom: DomSanitizer) { }
 
   setSlide(slideId: number) {
     this.carousel?.select(slideId.toString());
   }
   ngOnInit(): void {
-
+    this.safeUrl = this.dom.bypassSecurityTrustUrl(`whatsapp://send?text=${this.router['location']._platformLocation.location.href}`);
     this.refresh();
   }
   private refresh() {
